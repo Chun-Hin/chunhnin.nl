3ea6eab44d7c71ac2cd78f1251199663c4b6a90a
Chun Hin Ip <100929435+Chun-Hin@users.noreply.github.com>
2023-04-24 23:33:51 +0200

    responsive bugs fixed

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import chunfoto from '../images/chunfoto.JPG'
 
 function About() {
     return (
-        <div className="w-full bg-white py-16 px-4" id="About">
+        <div className="w-full bg-white py-16 px-4 scroll-mt-20" id="About">
             <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
                 <img className="w-[450px] mx-auto my-4 rounded-lg" src={chunfoto} alt="/" />
                 <div className="flex flex-col justify-center">
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Contact() {
     return(
-        <div className="w-full py-16 text-white px-4 text-center flex flex-col justify-center items-center text-center" id="Contact">
+        <div className="h-screen w-full py-16 text-white px-4 text-center flex flex-col justify-center items-center" id="Contact">
             <div className="max-w-[1240px] mx-auto">
-                <div className="lg:col-span-2 my-4">
+                <div className="lg:col-span-2 my-4 items-center text-center">
                     <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">Dou you want to contact me?</h1>
-                    <p className="max-w-[450px] my-4 ml-4">You can always contact my by sending me an e-mail. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
+                    <p className="max-w-[450px] md:ml-4 my-4">You can always contact my by sending me an e-mail. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
                     <a href="mailto:chunhin.ip05@gmail.com" className="bg-[#00df9a] text-black rounded-lg font-medium w-[200px] mt-8 px-5 py-3">Say Hi!</a>
                 </div>
             </div>
