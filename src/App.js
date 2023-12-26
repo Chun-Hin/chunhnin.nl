cbe3d0f99731582d8134131b8fc4ce3e69f32503
Chun Hin Ip <100929435+Chun-Hin@users.noreply.github.com>
2023-12-26 22:25:40 +0100

    website in good state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Bars/Navbar';
-import Homepage from './components/Pages/Homepage';
-import Blog from './components/Blog';
-import SEO from './components/SEO';
-import LeftSidebar from "./components/Bars/LeftSidebar";
-import RightSidebar from "./components/Bars/RightSidebar";
-
-
+import Navbar from "./components/Navbar";
+import Homepage from "./components/Home";
+import About from "./components/About"
+import Contact from "./components/Contact";
 function App() {
     return (
-        <BrowserRouter>
-            <div className="flex flex-col md:flex-row">
-                <LeftSidebar />
-                <div className="flex-1">
-                    <Navbar />
-                    <Routes>
-                        <Route path="/" element={<Homepage />} />
-                        <Route path="/blog/*" element={<Blog />} />
-                        <Route path="/seo/*" element={<SEO />} />
-                    </Routes>
-                </div>
-                <RightSidebar />
-            </div>
-        </BrowserRouter>
+        <div>
+            <Navbar></Navbar>
+            <Homepage></Homepage>
+            <About></About>
+            <Contact></Contact>
+        </div>
     );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,34 +4,32 @@ import { Typewriter } from 'react-simple-typewriter'
 
 function Home() {
     return (
-        <>
-            <div className="text-white">
-                <div className="max-w-[900px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-                    <p className="text-[#00df9a] font-bold p-2">
-                        HI, MY NAME IS
+        <div className="text-white">
+            <div className="max-w-[900px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+                <p className="text-[#00df9a] font-bold p-2">
+                    HI, MY NAME IS
+                </p>
+                <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-4">
+                    Chun-Hin Ip.
+                </h1>
+                <div className="flex justify-center items-center">
+                    <p className="md:text-5xl sm:text-2xl text-xl font-bold py-4 text-gray-300">
+                        I'm going to build things for the
                     </p>
-                    <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-4">
-                        Chun-Hin Ip.
-                    </h1>
-                    <div className="flex justify-center items-center">
-                        <p className="md:text-5xl sm:text-2xl text-xl font-bold py-4 text-gray-300">
-                            I'm going to build things for the
-                        </p>
-                        <span className="md:text-5xl sm:text-2xl text-xl font-bold md:pl-4 pl-2 text-gray-300 -ml-1">
+                    <span className="md:text-5xl sm:text-2xl text-xl font-bold md:pl-4 pl-2 text-gray-300 -ml-1">
                         <Typewriter
-                            words={['web.', 'net.']}
+                            words={['internet.', 'web.', 'net.']}
                             loop={0}
                             typeSpeed={100}
                             deleteSpeed={75}
                         />
                     </span>
-                    </div>
-                    <p className="md:text-2xl sm:text-1xl text-xl font-bold text-gray-500 px-4">I’m a 3rd year student software
-                        developer located at Deltion College in Zwolle. Currently, I’m learning everything about
-                        coding.</p>
                 </div>
+                <p className="md:text-2xl sm:text-1xl text-xl font-bold text-gray-500 px-4">I’m a 3rd year student software
+                    developer located at Deltion College in Zwolle. Currently, I’m learning everything about
+                    coding.</p>
             </div>
-        </>
+        </div>
     );
 }
 
