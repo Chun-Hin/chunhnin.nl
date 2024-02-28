026527e33e3071b289bf44cce661bfa909d10031
Chun Hin Ip <100929435+Chun-Hin@users.noreply.github.com>
2024-02-28 12:59:14 +0100

    CV updated, Home page updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,8 +26,7 @@ function Home() {
                     </span>
                 </div>
                 <p className="md:text-2xl sm:text-1xl text-xl font-bold text-gray-500 px-4">I’m a 3rd year student software
-                    developer located at Deltion College in Zwolle. Currently, I’m learning everything about
-                    coding.</p>
+                    developer located at Deltion College in Zwolle. Currently passed my exam and finishing my internship at the moment.</p>
             </div>
         </div>
     );
